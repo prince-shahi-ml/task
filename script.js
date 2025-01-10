073fe6aa7077a56560daab83cda545314e1d885a
Prince Shahi <prince.shahi@beetechsolution.com>
2025-01-10 12:10:45 +0545

    next commit

---
+++ script.js
@@ -0,0 +1,25 @@
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: false,
+    dots: false,
+    autoplay: true,
+    slideTransition: 'linear',
+    autoplayTimeout: 2000,
+    autoplaySpeed: 8000,
+    autoplayHoverPause: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 3
+        },
+        1000: {
+            items: 4
+        },
+        1200: {
+            items: 5
+        }
+    }
+})
